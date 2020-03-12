480ad14a745fc3899c02fed5c62ebbceecfd6d04
박선우 <jinwon11022@naver.com>
2020-03-12 23:37:31 +0900

    Array

---
+++ 200312/src/200312.java
@@ -0,0 +1,9 @@
+public class java{
+    public static void main(Strring[] args){
+        String[] class = {"박선우", "김선우", "이선우"};
+        for (int i=0;i<class.length;i++){
+            String class = class[i];
+            System.out.println(class[i]+"가 상담을 받았습니다.");
+        }
+    }
+}
