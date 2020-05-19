df7e962791ab7b9b90e835668bebb6aabb6ac2dc
박선우 <jinwon11022@naver.com>
2020-05-19 16:19:25 +0900

    ADD CODE

--- 200519/new.java
+++ 200519/new.java
@@ -3,9 +3,9 @@ class main{
         String = name;
         java.util.Scanner input= new java.util.Scanner(System.in);
         Overloading a = new Overloading();
-        Scanner sc = new Scanner (System.in);
-        System.out.println("name:");
-        name = sc.next();
-        System.out.println("name:"+name);
+
     }
+}
+class Overloading {
+    
 }
