875087a4c5a37522a8312ed9f38851d1c276ed54
박선우 <jinwon11022@naver.com>
2020-05-19 15:45:49 +0900

    Write Code

---
+++ 200519/new.java
@@ -0,0 +1,11 @@
+class main{
+    public static void main (String[] args){
+        String = name;
+        java.util.Scanner input= new java.util.Scanner(System.in);
+        Overloading a = new Overloading();
+        Scanner sc = new Scanner (System.in);
+        System.out.println("name:");
+        name = sc.next();
+        System.out.println("name:"+name);
+    }
+}
