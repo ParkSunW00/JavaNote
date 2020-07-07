82072b31b013660835bce28f3b2bbe81e29771b3
박선우 <psw110222@gmail.com>
2020-07-07 12:25:54 +0900

    Unfinished code

---
+++ 0706/src/wls.java
@@ -0,0 +1,10 @@
+package Event;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseListener;
+
+public class wls extends JFrame implements MouseListener {
+
+    Container panel=getContentPane();
+    JButton btn=new JButton("버튼");
+}
